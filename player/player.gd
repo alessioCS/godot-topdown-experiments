f9e9b3d2313ad3dd23134a51d28227498024aed9
Alessio CS <alessiocats@gmail.com>
2025-06-17 21:47:22 -0400

    Camera Bounds

--- player/player.gd
+++ player/player.gd
@@ -4,15 +4,13 @@ var speed = 0
 const WALK_SPEED = 60
 const RUN_SPEED = 100
 
+@onready var camera_2d: Camera2D = $Camera2D
 @onready var sprite = $AnimatedSprite2D
 var input_direction = Vector2.ZERO
 
 ## State Machine
 enum States {IDLE, WALKING, RUN}
 var state: States = States.IDLE
-
-func _ready():
-	position = get_viewport_rect().size / 2
 
 func get_input():
 	input_direction = Input.get_vector("left", "right", "up", "down")
@@ -22,6 +20,21 @@ func handle_facing():
 		sprite.flip_h = false
 	if Input.is_action_pressed("left"):
 		sprite.flip_h = true
+
+func _ready():
+	camera_2d.make_current()
+	call_deferred("limit_camera")
+
+func limit_camera():
+	var ground = get_tree().get_nodes_in_group("Ground")[0]
+	var map_limit : Rect2i
+	map_limit = ground.get_used_rect()
+	var tile_size = Vector2i(16, 16)
+
+	camera_2d.limit_left = map_limit.position.x * tile_size.x
+	camera_2d.limit_top = map_limit.position.y * tile_size.y
+	camera_2d.limit_right = (map_limit.position.x + map_limit.size.x) * tile_size.x
+	camera_2d.limit_bottom = (map_limit.position.y + map_limit.size.y) * tile_size.y
 
 func _physics_process(delta):
 	get_input()
