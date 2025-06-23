9ffe6eba774537c1e19ee6ebc8dc93d3e1c8d0a9
Alessio CS <alessiocats@gmail.com>
2025-06-22 23:25:21 -0400

    Revert "Changed camera control to be external from player"
    This reverts commit 323ed65f4c20f68f1ebab580d69404d1f4115c75.

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @onready var front_ray: RayCast2D = $front_ray
-#@onready var camera_2d: Camera2D = $Camera2D
+@onready var camera_2d: Camera2D = $Camera2D
 @onready var sprite = $AnimatedSprite2D
 
 var speed = 0
@@ -20,9 +20,8 @@ enum States {IDLE, WALKING, RUN, TALKING}
 var state: States = States.IDLE
 
 func _ready():
-	#camera_2d.make_current()
-	#call_deferred("limit_camera")
-	pass
+	camera_2d.make_current()
+	call_deferred("limit_camera")
 
 ## Gets normalized movement vector
 func movement():
@@ -57,17 +56,16 @@ func handle_facing():
 		sprite.flip_h = true
 
 ## Camera Limiting Tool
-#func limit_camera():
-	#var ground = get_tree().get_nodes_in_group("Ground")[0]
-	#var map_limit : Rect2i
-	#map_limit = ground.get_used_rect()
-	#var tile_size = Vector2i(16, 16)
-#
-	#camera_2d.limit_left = map_limit.position.x * tile_size.x
-	#camera_2d.limit_top = map_limit.position.y * tile_size.y
-	#camera_2d.limit_right = (map_limit.position.x + map_limit.size.x) * tile_size.x
-	#camera_2d.limit_bottom = (map_limit.position.y + map_limit.size.y) * tile_size.y
+func limit_camera():
+	var ground = get_tree().get_nodes_in_group("Ground")[0]
+	var map_limit : Rect2i
+	map_limit = ground.get_used_rect()
+	var tile_size = Vector2i(16, 16)
 
+	camera_2d.limit_left = map_limit.position.x * tile_size.x
+	camera_2d.limit_top = map_limit.position.y * tile_size.y
+	camera_2d.limit_right = (map_limit.position.x + map_limit.size.x) * tile_size.x
+	camera_2d.limit_bottom = (map_limit.position.y + map_limit.size.y) * tile_size.y
 
 ## Called every frame
 func _physics_process(delta):
