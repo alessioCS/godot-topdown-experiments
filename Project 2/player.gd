fa46427201aa10aebaa16d6dfebb3c1587645da2
Alessio CS <alessiocats@gmail.com>
2025-06-27 03:19:44 -0400

    Player directional sprite flip

--- Project 2/player.gd
+++ Project 2/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 
 const IDLE_SPEED : float = 0.
-const MOVE_SPEED : float =  20.
-var facing_left := true
+const MOVE_SPEED : float =  60.
 var direction := Vector2.ZERO
+@onready var sprite = $AnimatedSprite2D
 
 enum States {IDLE, WALKING}
 var state := States.IDLE
 
 func _ready():
 	pass
+	
+func handle_facing():
+	if direction.x > 0:
+		sprite.flip_h = false
+	elif direction.x < 0:
+		sprite.flip_h = true
 	
 func get_direction():
 	return Input.get_vector("left", "right", "up", "down")
@@ -24,7 +30,10 @@ func _physics_process(delta):
 	
 	match state:
 		States.IDLE:
+			sprite.play("idle")
 			velocity = direction * IDLE_SPEED
 		States.WALKING:
+			sprite.play("walk")
 			velocity = direction * MOVE_SPEED
+	handle_facing()
 	move_and_slide()
